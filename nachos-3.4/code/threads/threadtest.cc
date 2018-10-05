efe585c798d34a0f674eb5f015084c17eb05b224
DantesYan <1500012820@pku.edu.cn>
2018-10-05 20:28:19 +0800

    lab1

---
+++ nachos-3.4/code/threads/threadtest.cc
@@ -0,0 +1,99 @@
+// threadtest.cc 
+//	Simple test case for the threads assignment.
+//
+//	Create two threads, and have them context switch
+//	back and forth between themselves by calling Thread::Yield, 
+//	to illustratethe inner workings of the thread system.
+//
+// Copyright (c) 1992-1993 The Regents of the University of California.
+// All rights reserved.  See copyright.h for copyright notice and limitation 
+// of liability and disclaimer of warranty provisions.
+
+#include "copyright.h"
+#include "system.h"
+
+// testnum is set in main.cc
+int testnum = 1;
+
+//----------------------------------------------------------------------
+// SimpleThread
+// 	Loop 5 times, yielding the CPU to another ready thread 
+//	each iteration.
+//
+//	"which" is simply a number identifying the thread, for debugging
+//	purposes.
+//----------------------------------------------------------------------
+
+void TS(){
+
+    printf("Threads Information:\n");
+    for(int i = 0;i < 128;++i){
+        if(tid_flag[i] != 0){
+            printf("tid: %d, uid: %d, name: %s \n", tid_pointer[i]->get_tid(), tid_pointer[i]->get_uid(), tid_point[i]->getName());
+        }
+    }
+}
+
+void
+SimpleThread(int which)
+{
+    int num;
+    
+    for (num = 0; num < 5; num++) {
+	printf("*** thread %d looped %d times\n", which, num);
+        currentThread->Yield();
+    }
+}
+
+//----------------------------------------------------------------------
+// ThreadTest1
+// 	Set up a ping-pong between two threads, by forking a thread 
+//	to call SimpleThread, and then calling SimpleThread ourselves.
+//----------------------------------------------------------------------
+
+void
+ThreadTest1()
+{
+    DEBUG('t', "Entering ThreadTest1");
+
+    Thread *t = new Thread("forked thread");
+
+    t->Fork(SimpleThread, 1);
+    SimpleThread(0);
+}
+
+void
+ThreadTest2()
+{
+    DEBUG('t', "Entering ThreadTest1");
+    
+    for(int i = 0; i < 130; i++)
+        print(i)
+        Thread *t = new Thread("forked thread");
+    
+    print("Call TS Function:")
+    TS();
+
+}
+
+//----------------------------------------------------------------------
+// ThreadTest
+// 	Invoke a test routine.
+//----------------------------------------------------------------------
+
+void
+ThreadTest()
+{
+    switch (testnum) {
+    case 1:
+	ThreadTest1();
+	break;
+    case 2:
+    ThreadTest2();
+    break;
+    default:
+	printf("No test specified.\n");
+	break;
+    }
+}
+
