cfabe9e129df8d5464a3b89ed104c78f8f04eb1e
DantesYan <1500012820@pku.edu.cn>
2018-10-05 20:32:05 +0800

    lab1

--- nachos-3.4/code/threads/thread.h
+++ nachos-3.4/code/threads/thread.h
@@ -79,9 +79,9 @@ class Thread {
     // THEY MUST be in this position for SWITCH to work.
     int* stackTop;			 // the current stack pointer
     int machineState[MachineStateSize];  // all registers except for stackTop
-    Thread(char* debugName);        // initialize a Thread 
     
   public:
+    Thread(char* debugName);        // initialize a Thread 
     ~Thread(); 				// deallocate a Thread
 					// NOTE -- thread being deleted
 					// must not be running when delete 
